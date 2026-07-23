15370aff32dd00b79cae0d5bc50bc75af6b2176f
NGAW <ngaw.mobile.dev@gmail.com>
2026-07-24 00:40:00 +1200

    day 03 - arrays

--- src/main.rs
+++ src/main.rs
@@ -13,41 +13,39 @@ enum Duration {
 }
 
 fn main() {
-    let sub1 = Subscription {
-        title: "Github".to_string(),
-        price: 999,
-        duration: Duration::Monthly,
-        start_date: "14/04/2026".to_string(),
-    };
-    let sub2 = Subscription {
-        title: "OpenCode".to_string(),
-        price: 250,
-        duration: Duration::Weekly,
-        start_date: "22/03/2026".to_string(),
-    };
+    let subscriptions = [
+        Subscription {
+            title: "Github".to_string(),
+            price: 999,
+            duration: Duration::Monthly,
+            start_date: "14/04/2026".to_string(),
+        },
+        Subscription {
+            title: "OpenCode".to_string(),
+            price: 250,
+            duration: Duration::Weekly,
+            start_date: "22/03/2026".to_string(),
+        },
+    ];
 
-    // We want the price to be printed as $ price / 100
-    let dollar_price: f32 = sub1.price as f32 / 100.0;
-    println!(
-        "Your subscription for {} at ${dollar_price} per {:?} starting {}",
-        sub1.title, sub1.duration, sub1.start_date
-    );
-    println!(
-        "Your {} subscription yearly cost is ${}",
-        sub1.title,
-        calculate_yearly_cost(sub1.price, sub1.duration)
-    );
+    let total_cost: f32;
+    for sub in subscriptions {
+        // We want the price to be printed as $ price / 100
+        let dollar_price: f32 = sub.price as f32 / 100.0;
+        println!(
+            "Your subscription for {} at ${dollar_price} per {:?} starting {}",
+            sub.title, sub.duration, sub.start_date
+        );
+        println!(
+            "Your {} subscription yearly cost is ${}",
+            sub.title,
+            calculate_yearly_cost(sub.price, sub.duration)
+        );
 
-    let dollar_price2: f32 = sub2.price as f32 / 100.0;
-    println!(
-        "Your subscription for {} at ${dollar_price2} per {:?} starting {}",
-        sub2.title, sub2.duration, sub2.start_date
-    );
-    println!(
-        "Your {} subscription yearly cost is ${}",
-        sub2.title,
-        calculate_yearly_cost(sub2.price, sub2.duration)
-    );
+        total_cost = total_cost + calculate_yearly_cost(sub.price, sub.duration);
+    }
+
+    println!("Total yearly cost ${total_cost}")
 }
 
 fn calculate_yearly_cost(price: i32, duration: Duration) -> f32 {
