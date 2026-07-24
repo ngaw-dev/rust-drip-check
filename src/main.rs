cb4fdb9bfc68ee026f9507d27b21a3b67e0239da
NGAW <ngaw.mobile.dev@gmail.com>
2026-07-24 22:52:37 +1200

    08 - map subscriptions

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use rust_drip_check::Subscription;
+use rust_drip_check::{get_total_cost, Subscription};
 
 fn main() {
     let file_path = "data.csv";
-
     let mut reader = csv::Reader::from_path(file_path).expect("ERROR: Could not open csv");
+
+    let mut subscriptions = Vec::new();
 
     for data in reader.deserialize() {
         let record: Subscription = data.expect("ERROR: Could not read record");
-        println!("{:?}", record);
+        subscriptions.push(record);
     }
+
+    get_total_cost(&subscriptions);
 }
