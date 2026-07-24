6701af0e37c243769789689a2bb17d25e812aaa0
NGAW <ngaw.mobile.dev@gmail.com>
2026-07-24 22:26:53 +1200

    08 - read files

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,10 @@
-use rust_drip_check::{get_total_cost, Duration, Subscription};
+use std::fs;
 
 fn main() {
-    let mut subscriptions = Vec::new();
+    let file_path = "data.csv";
 
-    subscriptions.push(Subscription {
-        title: "Github".to_string(),
-        price: 999,
-        duration: Duration::Monthly,
-        start_date: "14/04/2026".to_string(),
-    });
-    subscriptions.push(Subscription {
-        title: "OpenCode".to_string(),
-        price: 250,
-        duration: Duration::Weekly,
-        start_date: "22/03/2026".to_string(),
-    });
-    subscriptions.push(Subscription {
-        title: "Amazon Prime".to_string(),
-        price: 3000,
-        duration: Duration::Yearly,
-        start_date: "08/10/2025".to_string(),
-    });
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
 
-    get_total_cost(&subscriptions);
-
-    println!("-- Removing the subscriptions at index 1 (start from 0) --");
-    subscriptions.remove(1);
-    get_total_cost(&subscriptions);
-
-    println!("-- Adding new subscriptions --");
-    subscriptions.push(Subscription {
-        title: "Netflix".to_string(),
-        price: 1549,
-        duration: Duration::Monthly,
-        start_date: "23/08/2025".to_string(),
-    });
-    get_total_cost(&subscriptions);
-
-    println!("-- Show subscriptions where yearly price is > $5.00 --");
-    let large_subscriptions = subscriptions.iter().filter(|sub| sub.price > 500);
-    for sub in large_subscriptions {
-        sub.display();
-    }
-
-    println!("-- Show subscriptions Monthly --");
-    let monthly = subscriptions
-        .iter()
-        .filter(|sub| matches!(sub.duration, Duration::Monthly));
-    for sub in monthly {
-        sub.display();
-    }
-
-    println!("-- Show subscriptions with title like git --");
-    let filtered = subscriptions
-        .iter()
-        .filter(|sub| sub.title.to_lowercase().contains("git"));
-    for sub in filtered {
-        sub.display();
-    }
+    println!("{contents}");
 }
