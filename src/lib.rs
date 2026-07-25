0a2d9d12a5f55ce7ed932541e89b6830f9f05a10
NGAW <ngaw.mobile.dev@gmail.com>
2026-07-25 16:12:24 +1200

    day 10 - remove dead code

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,3 @@
 pub mod db;
 pub mod models;
 pub mod schema;
-pub mod subscription;
-
-pub use subscription::{Duration, Subscription};
-
-pub fn get_total_cost(subscriptions: &Vec<Subscription>) {
-    let mut total_cost: f32 = 0.0;
-    for sub in subscriptions {
-        println!(
-            "Your subscription for {} at ${} per {:?} starting {}",
-            sub.title,
-            sub.dollar_price(),
-            sub.duration,
-            sub.start_date
-        );
-        let yearly_cost = sub.yearly_cost();
-        println!(
-            "Your {} subscription yearly cost is ${}",
-            sub.title, yearly_cost
-        );
-
-        total_cost = total_cost + yearly_cost;
-    }
-
-    println!("Total yearly cost ${total_cost}");
-}
