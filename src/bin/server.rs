8ff08caaae331ec790aeb74f3b5bb03e5ef161cc
NGAW <ngaw.mobile.dev@gmail.com>
2026-07-26 00:06:52 +1200

    day 11 - return raw json data

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,14 +1,26 @@
 use axum::{
-    routing::get,
-    Router,
+    Json, Router,
+    routing::{delete, get, post, put},
 };
+use serde_json::{Value, json};
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().route("/", get(get_subscription));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn get_subscription() -> Json<Value> {
+    Json(json!({ 
+        "data": {
+            "id": 1,
+            "title": "Subscriptions title",
+            "price": 100,
+            "duration": "Monthly",
+            "start_date": "2026-04-14",
+    } }))
 }
