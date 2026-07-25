cde4181a8cfa644b8abe0d4ed0a3d018f7eaf13f
NGAW <ngaw.mobile.dev@gmail.com>
2026-07-26 00:31:44 +1200

    day 11 - add nested routes

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,3 +1,6 @@
+#[derive(Clone)]
+struct AppState;
+
 use axum::{
     Json, Router,
     routing::{delete, get, post, put},
@@ -7,14 +10,29 @@ use serde_json::{Value, json};
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(get_subscription));
+    let app = Router::new()
+        .route("/", get(|| async { "Visit api endpoints for data" }))
+        .nest("/api/v1/subscriptions", subscription_routes())
+        .with_state(AppState);
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn get_subscription() -> Json<Value> {
+fn subscription_routes() -> Router<AppState> {
+    Router::new()
+        .route("/", get(show_subscriptions).post(create_subscription))
+        .route(
+            "/{id}",
+            get(get_subscription)
+                .post(create_reminder)
+                .put(update_subscription)
+                .delete(delete_subscription),
+        )
+}
+
+async fn show_subscriptions() -> Json<Value> {
     Json(json!({ 
         "data": {
             "id": 1,
@@ -23,4 +41,36 @@ async fn get_subscription() -> Json<Value> {
             "duration": "Monthly",
             "start_date": "2026-04-14",
     } }))
+}
+
+async fn create_subscription() -> Json<Value> {
+    Json(json!({
+        "data": {
+            "key": "TODO: create_subscription"
+        }
+    }))
+}
+
+async fn get_subscription() -> Json<Value> {
+    Json(json!({
+        "data": {
+            "key": "TODO: get_subscription"
+        }
+    }))
+}
+
+async fn update_subscription() -> Json<Value> {
+    Json(json!({
+        "data": {
+            "key": "TODO: update_subscription"
+        }
+    }))
+}
+
+async fn delete_subscription() -> Json<Value> {
+    Json(json!({
+        "data": {
+            "key": "TODO: delete_subscription"
+        }
+    }))
 }
